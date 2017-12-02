d30f55506d35e263bbcd502d81d65d03503395e8
Evan Krambuhl <thekrambuhl@gmail.com>
2017-12-02 10:44:22 -0800

    Update rendering logic

--- examples/webpack-vue-example/source/archive.js
+++ examples/webpack-vue-example/source/archive.js
@@ -9,7 +9,10 @@ import {
   registerHelpers,
   archiveHelpers
 } from 'reflecto-archive'
+
+import Vue from 'vue'
 import Handlebars from 'handlebars/runtime'
+
 import * as Categories from '@shared/categories'
 
 const {
@@ -59,13 +62,26 @@ export const schema =
     filter: def => def.elementType === 'tags',
     groups: [{
       filter: def => def.element.category === Categories.Default
+    }, {
+      title: 'Forms',
+      filter: def => def.element.category === Categories.Form
     }]
   }])
 
 // Provide render fn for browser
 export const renderComponent = (el, component, engine = 'vue') => {
-  new Vue({
-    el,
-    render: component
+  if (el._vm) {
+    el._vm.$destroy(true)
+    el._vm.$el.remove()
+    el._vm = null
+  }
+
+  const container = document.createElement('div')
+  el.appendChild(container)
+  el._vm = new Vue({
+    el: container,
+    render: typeof component === 'function'
+      ? component
+      : h => h(component)
   })
 }
